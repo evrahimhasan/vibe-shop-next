1d5cbd03acbb5d77cd689bf2d5de5eb507498995
evrahimhasan <evrahimhasanemran.com>
2026-01-18 13:10:46 +0600

    added LoadingSpinner

---
+++ src/app/LoadingSpinner.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const LoadingSpinner = () => {
+    return (
+        <div>
+            <div className='min-h-screen flex justify-center items-center text-8xl'>
+                <h2>L</h2> <span>🎯</span>
+                <h2>LOADING</h2>
+            </div>
+        </div>
+    );
+};
+
+export default LoadingSpinner;
